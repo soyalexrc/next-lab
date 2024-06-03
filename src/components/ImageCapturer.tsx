42e27947d9e7f3509c885ebc9f9b092c93661379
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-02 21:17:08 -0500

    added some visual assitance

--- src/components/ImageCapturer.tsx
+++ src/components/ImageCapturer.tsx
@@ -5,21 +5,33 @@ import Image from "next/image";
 
 export default function ImageCapturer() {
     const [result, setResult] = useState<any>()
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<any>(null);
+    const [debug, setDebug] = useState<any>({})
+
     const triggerInput = () => {
         const input = document.getElementById('pictureInput');
         input?.click();
     }
 
     const takePicture = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLoading(true);
         const file = event.target.files![0];
         const fileUrl = URL.createObjectURL(file);
         const formData = new FormData();
+
+
 
         formData.append('image', file)
 
         const fileBase64: any = await toBase64(file)
 
-        const response = await fetch('/api/image-text-detector/analyzeimage', {
+        setDebug({
+            files: event.target.files,
+            imageBase64: fileBase64
+        });
+
+        await fetch('/api/image-text-detector/analyzeimage', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -27,11 +39,15 @@ export default function ImageCapturer() {
             body: JSON.stringify({
                 image: fileBase64.split(',')[1]
             })
-        });
+        }).then(async response => {
+            const json = await response.json();
+            setResult(json);
+        }).catch(err => {
+            setError(err);
+        }).finally(() => {
+            setLoading(false)
+        })
 
-        const json = await response.json();
-        console.log(json);
-        setResult(json);
     }
 
         const toBase64 = (file: File) => {
@@ -50,11 +66,27 @@ export default function ImageCapturer() {
             });
         };
 
+    if (loading) {
+        return (
+            <div>loading...</div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div>
+                {JSON.stringify(error, null, 2)}
+            </div>
+        )
+    }
+
 
     return(
         <div>
             <input id='pictureInput' type="file" accept="image/*" capture="environment" className='hidden' onChange={takePicture}/>
             <button onClick={triggerInput}>take picture</button>
+
+            <pre>{JSON.stringify(debug, null, 2)}</pre>
 
             {
                 result &&
